0dfb5d196ae75aaecf565d8c7537b9f45c8e1ade
Anastasia Fedulova <fedulova.a.m@bk.ru>
2025-03-06 18:30:26 +0300

    first task

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -1,11 +1,15 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 
 public class CardOrderTest {
     private WebDriver driver;
@@ -22,6 +26,8 @@ public class CardOrderTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+
+        driver.get("http://localhost:9999");
     }
 
     @AfterEach
@@ -32,7 +38,15 @@ public class CardOrderTest {
 
     @Test
     void shouldSubmitForm() {
-
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        form.findElement(By.cssSelector("[data-test-id='name'] .input__control")).sendKeys("Петрова Анна");
+        form.findElement(By.cssSelector("[data-test-id='phone'] .input__control")).sendKeys("+79998881122");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        WebElement actual = driver.findElement(By.cssSelector("[data-test-id='order-success']"));
+        assertTrue(actual.isDisplayed());
+        assertEquals(
+                "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",
+                actual.getText().trim());
     }
-
 }
